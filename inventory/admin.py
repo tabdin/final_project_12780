675a86465918a50438afec5c3c6712fcec71b593
tabdin <tabdin@andrew.cmu.edu>
2025-12-14 17:27:23 -0500

    feat: register models in admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# Based on admin.py in helloworld/pittrain
+from .models import Equipment, Checkout 
+
 # Register your models here.
+admin.site.register(Equipment)
+admin.site.register(Checkout)
